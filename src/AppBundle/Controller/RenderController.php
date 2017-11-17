c86310365e37dd2845f5ad51dc9edd6e5b3f8035
primus852 <primus852@users.noreply.github.com>
2017-11-17 12:00:15 +0100

    Create RenderController.php

---
+++ src/AppBundle/Controller/RenderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RenderController extends Controller
+{
+
+    /**
+     * @Route("/_render/_overview", name="renderOverview")
+     * @param $request Request
+     * @return Response
+     */
+    public function mainAction(Request $request)
+    {
+
+        /* Check Login Status */
+        if (($checkLogin = $this->get('app.helper')->checkLogin($request->get('_route'))) !== true) {
+            return $checkLogin;
+        }
+
+        return $this->render(
+            ':default:index.html.twig',
+            array(
+            )
+        );
+    }
+
+}
